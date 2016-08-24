6cb3d898110dc69519170ac9e4a5936a583eb3e3
Ross Golder <ross@golder.org>
2016-08-24 12:32:38 +0700

    No JVM memory alloc requirements. Don't rebuild each environment on restart.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,13 @@ cat >/etc/puppetmaster.conf <<EOF
 export GITREPO=$GITREPO
 EOF
 
+# Tell JVM not to enforce memory allocation
+cat >>/etc/default/puppetserver <<EOF
+JAVA_ARGS=""
+EOF
+
 # Update and re-build each environment
-/usr/local/bin/puppetmaster-deployfiles
+#/usr/local/bin/puppetmaster-deployfiles
 
 exec "$@"
 
